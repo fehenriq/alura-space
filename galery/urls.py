b4ef160ab9035460f77a6b42722692d67626fbfe
Felipe Rodrigues <lipe.h.r@gmail.com>
2023-01-13 11:53:39 -0300

    second django 4 course finished

--- galery/urls.py
+++ galery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galery.views import index, image
+from galery.views import index, image, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', image, name='imagem')
+    path('image/<int:foto_id>', image, name='image'),
+    path('buscar', buscar, name="buscar")
 ]
--- galery/views.py
+++ galery/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galery.models import Fotografia
+
 
 def index(request):
-  return render(request, 'galery/index.html')
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request, "galery/index.html", {"cards": fotografias})
 
-def image(request):
-  return render(request, 'galery/imagem.html')
+
+def image(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, "galery/imagem.html", {"fotografia": fotografia})
+
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, "galery/buscar.html", {"cards": fotografias})
